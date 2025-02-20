9d4ff73cf8b21270518bb2f4dc46250912c62c65
adrianosalge <asalge@terra.com.br>
2025-02-20 09:24:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ import time
 while True:  
     print("🤖 O robô está funcionando!")  
     time.sleep(10)  # Espera 10 segundos antes de repetir  
+    print("🤖 O código do robô foi enviado corretamente!")
+    time.sleep(10)  # Espera 10 segundos antes de repetir  
